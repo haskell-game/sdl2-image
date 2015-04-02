1c341db74e450d089372879a16e974c02bd563a8
sbidin <sinisa@bidin.cc>
2015-04-02 16:27:04 +0200

    Add example executable.

---
+++ example/Example.hs
@@ -0,0 +1,10 @@
+module Main where
+
+-- import qualified SDL
+import qualified SDL.Image as IMG
+
+main :: IO ()
+main = do
+  -- SDL.initialize [SDL.InitEverything]
+  -- print =<< SDL.version
+  print =<< IMG.version
