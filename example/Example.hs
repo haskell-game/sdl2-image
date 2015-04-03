4402d2bb2453e4e4a1f52d1fe1cb1852a6d7147f
sbidin <sinisa@bidin.cc>
2015-04-03 10:12:37 +0200

    Add working example.

--- example/Example.hs
+++ example/Example.hs
@@ -1,12 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
--- import qualified SDL
+import System.Environment (getArgs)
+import Control.Concurrent (threadDelay)
+
+import qualified SDL
 import qualified SDL.Image as IMG
 
 main :: IO ()
 main = do
-  -- SDL.initialize [SDL.InitEverything]
-  -- print =<< SDL.version
-  print =<< IMG.version
-  IMG.initialize [IMG.InitPNG]
-  IMG.quit
+  SDL.initialize [SDL.InitVideo]
+  window <- SDL.createWindow "SDL_image test" SDL.defaultWindow
+
+  args <- getArgs
+  case args of
+    [] -> putStrLn "Usage: cabal run path/to/image.(png|jpg|...)"
+    (path:_) -> do
+      putStrLn $ "Loading and displaying " ++ path
+      IMG.initialize [IMG.InitPNG, IMG.InitJPG]
+      image <- IMG.load path
+      IMG.quit
+
+      screen <- SDL.getWindowSurface window
+      SDL.blitSurface image Nothing screen Nothing
+      SDL.updateWindowSurface window
+      SDL.showWindow window
+      SDL.freeSurface image
+      threadDelay $ 1000000*2
+
+  SDL.destroyWindow window
+  SDL.quit
